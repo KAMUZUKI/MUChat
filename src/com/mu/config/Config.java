f80c8510d72c5cf547c0282043637133ef18d7e2
KAMUZUKI <1437487442@qq.com>
2022-07-16 18:05:04 +0800

    用来存储配置文件信息

---
+++ src/com/mu/config/Config.java
@@ -0,0 +1,13 @@
+package com.mu.config;
+
+/**
+ * 用来存储配置文件信息
+ * @author MUZUKI
+ */
+public class Config {
+	public static String DOWNLOAD_PATH;
+	
+	public static String MESSAGE_PATH;
+	
+ 	public static int WINDOW_ALPHA;
+}
