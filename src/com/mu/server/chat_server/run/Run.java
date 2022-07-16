b7f9c2eef8c6797efcb84a4c720a69fdf42724b2
KAMUZUKI <1437487442@qq.com>
2022-07-16 18:16:31 +0800

    服务器启动管理窗口

---
+++ src/com/mu/server/chat_server/run/Run.java
@@ -0,0 +1,14 @@
+package com.mu.server.chat_server.run;
+
+import com.mu.server.chat_server.view.Server_Frame;
+
+/**
+ * 服务器启动管理窗口
+ * @author MUZUKI
+ *
+ */
+public class Run {
+	public static void main(String[] args) {
+		Server_Frame ServerFrame = new Server_Frame();
+	}
+}
